0bb06a92911ba1dd86b94192c9ab7d9636ac3be5
Ankit-2025-afk <ashu61545@gmail.com>
2026-03-23 05:14:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import ast
 import google.generativeai as genai
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 
 # 🔥 ADD YOUR API KEY HERE
 genai.configure(api_key="AIzaSyCuE9llM-3WJytnYJ1RjHHzeDdfgCgeHiQ")
